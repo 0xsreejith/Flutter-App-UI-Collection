a2647651291d62941679edd1c6598036634275f9
0xsreejith <csreejithofficail@gmail.com>
2024-12-06 10:02:41 +0530

    first screen completed

--- meditation_ui/lib/screens/meditation_screen.dart
+++ meditation_ui/lib/screens/meditation_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class MeditationScreen extends StatelessWidget {
@@ -15,7 +17,7 @@ class MeditationScreen extends StatelessWidget {
           Container(
             height: 300,
             width: double.infinity,
-            color: Color(0xffd4dcbc),
+            color: const Color(0xffd4dcbc),
           ),
           Positioned(
               top: 150,
@@ -24,7 +26,7 @@ class MeditationScreen extends StatelessWidget {
                 child: Container(
                   height: 400,
                   width: 400,
-                  decoration: BoxDecoration(
+                  decoration: const BoxDecoration(
                       color: Color(0xfffdeccd),
                       borderRadius:
                           BorderRadius.only(topLeft: Radius.circular(300))),
@@ -37,7 +39,7 @@ class MeditationScreen extends StatelessWidget {
                 child: Container(
                   height: 400,
                   width: 400,
-                  decoration: BoxDecoration(
+                  decoration: const BoxDecoration(
                       color: Color(0xfffedacc),
                       borderRadius:
                           BorderRadius.only(topLeft: Radius.circular(300))),
@@ -50,7 +52,7 @@ class MeditationScreen extends StatelessWidget {
                 child: Container(
                   height: 400,
                   width: 400,
-                  decoration: BoxDecoration(
+                  decoration: const BoxDecoration(
                       color: Colors.white,
                       borderRadius:
                           BorderRadius.only(topLeft: Radius.circular(300))),
@@ -62,7 +64,7 @@ class MeditationScreen extends StatelessWidget {
               top: 160,
               child: Image.network(
                 'https://widgetwisdom.com/wp-content/uploads/2024/12/Organic-Flat-People-Meditating-Illustration.png',
-                height: 350,
+                height: 300,
               )),
           Positioned(
               top: 140,
@@ -75,17 +77,53 @@ class MeditationScreen extends StatelessWidget {
               child: Image.network(
                   'https://widgetwisdom.com/wp-content/uploads/2024/12/Cloud-Icons.png')),
           Positioned(
-            top: 550,
-            left: 0,
-            right: 0,
+              left: 0,
+              right: 0,
+              top: 460,
+              child: RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                    text: 'Nature Your ',
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.bold),
+                    children: [
+                      TextSpan(
+                          text: 'Peaceful Mind ',
+                          style: TextStyle(
+                              fontSize: 25,
+                              color: Color(0xff583823),
+                              fontWeight: FontWeight.bold)),
+                      TextSpan(
+                          text: 'With',
+                          style: TextStyle(
+                              fontSize: 25,
+                              color: Color(0xff583823),
+                              fontWeight: FontWeight.bold)),
+                                    TextSpan(
+                          text: ' AI',
+                          style: TextStyle(
+                              fontSize: 25,
+                              color: Colors.black54,
+                              fontWeight: FontWeight.bold)),
+
+                    ]),
+              )),
+          Positioned(
+              top: 550,
+              left: 0,
+              right: 0,
               child: Container(
-            padding: EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 99, 7, 7),
-              shape: BoxShape.circle
-            ),
-            child: Icon(Icons.arrow_forward,color: Colors.white,),
-          ))
+                padding: const EdgeInsets.all(15),
+                decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 99, 7, 7),
+                    shape: BoxShape.circle),
+                child: const Icon(
+                  Icons.arrow_forward,
+                  color: Colors.white,
+                ),
+              ))
         ],
       ),
     );
